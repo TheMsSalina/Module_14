9d247f9e030a5cfd4085a59d8e68971028e026d6
TheMsSalina <salinar@inbox.ru>
2025-01-07 14:23:01 +0300

    Update crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -15,18 +15,50 @@ def initiate_db():
 #        cursor.execute("INSERT INTO Products (title, description, price) VALUES (?, ?, ?)",
 #            (f'Продукт {i}', f'Описание {i}', f'Цена: {i*100}'))
 
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL)
+    ''')
+
     connection.commit()
     connection.close()
-
-initiate_db()
 
 def get_all_products():
     connection = sqlite3.connect("bot_products.db")
     cursor = connection.cursor()
-    cursor.execute('SELECT title, description, price FROM Products')
+    cursor.execute('SELECT id, title, description, price FROM Products')
     products_db = cursor.fetchall()
     connection.commit()
     connection.close()
     return list(products_db)
 
+def populate_db():
+    cursor.executescript('''
+        INSERT INTO Products (title, description, price) VALUES (
+            "VitaC",
+            "Витамин С, в капсулах, 90 шт.",
+            40);
+        INSERT INTO Products (title, description, price) VALUES (
+            "VitaB",
+            "Витамин В, в капсулах, 180 шт.",
+            32);
+        INSERT INTO Products (title, description, price) VALUES (
+            "Omega-3",
+            "Омега-3, в капсулах, 90 шт.",
+            20);
+        INSERT INTO Products (title, description, price) VALUES (
+            "L-karn",
+            "L-карнитин, в капсулах, 30 шт.",
+            30);
+    ''')
+    connection.commit()
+
+if __name__ == '__main__':
+    initiate_db()
+    populate_db()
+    connection.close()
 
